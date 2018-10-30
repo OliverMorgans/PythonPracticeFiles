db411da4b2b5556bba00196dc7275137095f021b
Oliver Morgans <olivermorgans@gmail.com>
2018-10-30 16:19:09 +0000

    Updated code in all files to work

--- Number Guesser.py
+++ Number Guesser.py
@@ -1,22 +1,21 @@
 def guess_number():
   import random
   Answer = random.randint(0,11) 
-  print "Guess what number I'm thinking of between 0 and 10!"
-  Guess = input("Whats your guess? ")
+  print ("Guess what number I'm thinking of between 0 and 10!")
   correct = False
   
   while not correct:
-    Guess = input("Whats your guess? ")
+    Guess = int(input("Whats your guess? "))
     if Guess > 10:
-      print "thats not in the range"
+      print ("thats not in the range")
     elif Guess < 0:
-      print "thats not in the range"
+      print ("thats not in the range")
     elif Guess > Answer:
-      print "guess lower"
+      print ("guess lower")
     elif Guess < Answer:
-      print "guess higher"
+      print ("guess higher")
     elif Guess == Answer:
-      print "Congratulations you did it"
+      print ("Congratulations you did it")
       correct = True
 
 guess_number()
